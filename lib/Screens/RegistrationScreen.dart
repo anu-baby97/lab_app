e780d1b25ec30f8592a678c1ac1c3fdb13f2615c
anu-baby97 <anubbey@gmail.com>
2021-01-15 14:22:44 +0530

    (changed sdk version in pubspec)

--- lib/Screens/RegistrationScreen.dart
+++ lib/Screens/RegistrationScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:patient_portal/Screens/LoginScreen.dart';
 import 'package:patient_portal/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:firebase_core/firebase_core.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
